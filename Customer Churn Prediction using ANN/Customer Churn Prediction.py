86d61b38b18ac5adf96a61e434d4b80e49a89c0c
Gauree Dikkatwar <gdikkatwar@gmail.com>
2026-09-24 15:22:46 +0530

    Add ANN model for customer churn prediction with training, evaluation, and loss visualization

--- Customer Churn Prediction using ANN/Customer Churn Prediction.py
+++ Customer Churn Prediction using ANN/Customer Churn Prediction.py
@@ -19,7 +19,43 @@ X_test_scaled = scaler.transform(X_test)
 # Also Scalling is done.
 # Now, we are ready to build our model.
 
+import numpy as np
 import tensorflow
 from tensorflow import keras
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
+
+model = Sequential()
+
+model.add(Dense(3,activation='sigmoid',input_dim = 11))
+model.add(Dense(1,activation='sigmoid'))
+
+model.summary()
+print("_______________________________________________________________")
+
+model.compile(loss='binary_crossentropy',optimizer='Adam', metrics=['accuracy'])
+history = model.fit(X_train_scaled,y_train,epochs=10, validation_split=0.2)
+model.layers[0].get_weights()
+y_log = model.predict(X_test_scaled)
+y_pred = np.where(y_log > 0.5, 1,0)
+
+print("________________________________________________________________")
+print(y_pred)
+print("________________________________________________________________")
+
+# Check accuracy of model
+
+from sklearn.metrics import accuracy_score
+print("Model Accuracy : ",accuracy_score(y_test, y_pred))
+print("________________________________________________________________")
+
+import matplotlib.pyplot as plt
+
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.title('Training Loss vs Validation Loss')
+plt.legend()
+plt.show()
